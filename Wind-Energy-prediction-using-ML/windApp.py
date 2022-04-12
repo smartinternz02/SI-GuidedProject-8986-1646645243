9b78f5fe507138e58071a372f1afb89ebd03e5ca
JAGADEESH P <87519174+jagadeeshpj@users.noreply.github.com>
2022-04-12 17:50:55 +0530

    Update windApp.py

--- Wind-Energy-prediction-using-ML/windApp.py
+++ Wind-Energy-prediction-using-ML/windApp.py
@@ -1,44 +1,59 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+import matplotlib.pyplot as plt
 import joblib
-import requests
 
-app = Flask(__name__)
-model = joblib.load('power_prediction.sav')
+path = "data\T1.csv"
 
-@app.route('/')
-def home():
-    return render_template('intro.html')
+df = pd.read_csv(path)
 
-@app.route('/predict')
-def predict():
-    return render_template('predict.html')
+df.rename(columns={'Date/Time':'Time',
+                   'LV ActivePower (kW)':'ActivePower(kW)',
+                   "Wind Speed (m/s)":"WindSpeed(m/s)",
+                   "Wind Direction (°)":"Wind_Direction"},
+                    inplace=True)
+sns.pairplot(df)
 
-@app.route('/windapi',methods=['POST'])
-def windapi():
-    city=request.form.get('city')
-    apikey="86b1a085e43cad23bfd9c45d5fd88fc3"
-    url="http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apikey
-    resp = requests.get(url)
-    resp=resp.json()
-    temp = str((resp["main"]["temp"]) - 273.15) +" °C"
-    humid = str(resp["main"]["humidity"])+" %"
-    pressure = str(resp["main"]["pressure"])+" mmHG"
-    speed = str(resp["wind"]["speed"])+" m/s"
-    return render_template('predict.html', temp=temp, humid=humid, pressure=pressure,speed=speed)
-    
-@app.route('/y_predict',methods=['POST'])
-def y_predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    x_test = [[float(x) for x in request.form.values()]]
-    
-    prediction = model.predict(x_test)
-    print(prediction)
-    output=prediction[0]
-    return render_template('predict.html', prediction_text='The energy predicted is {:.2f} KWh'.format(output))
+corr = df.corr()
+plt.figure(figsize=(10, 8))
 
+ax = sns.heatmap(corr, vmin = -1, vmax = 1, annot = True)
+bottom, top = ax.get_ylim()
+ax.set_ylim(bottom + 0.5, top - 0.5)
+plt.show()
+corr
 
-if __name__ == "__main__":
-    app.run(debug=True)
+'''
+The heat map clearly tells us that there's no realtion between wind direction and 
+the Power generated but Wind speed, Theoritical power and Actual power generated 
+have a very positive correlation
+'''
+
+#df.drop(['Wind_Direction'],axis=1,inplace = True)
+df["Time"] = pd.to_datetime(df["Time"], format = "%d %m %Y %H:%M", errors = "coerce")
+df
+
+y = df['ActivePower(kW)'] #'Theoretical_Power_Curve (KWh)'
+X = df[['Theoretical_Power_Curve (KWh)','WindSpeed(m/s)']]#'ActivePower(kW)'
+
+from sklearn.model_selection import train_test_split
+train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 0)
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error,r2_score
+
+forest_model = RandomForestRegressor(max_leaf_nodes =500, random_state=1)
+forest_model.fit(train_X, train_y)
+
+power_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, power_preds))
+print(r2_score(val_y,power_preds))
+joblib.dump(forest_model, "power_prediction.sav")
